48354079acfdb92c9a80933e47442df2309f02da
trinh.ngoc.nam <namtrihh99@gmail.com>
2023-06-11 18:44:20 +0700

    Init

---
+++ AccountKeeper/AccountKeeper/AccountKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountKeeperApp.swift
+//  AccountKeeper
+//
+//  Created by NamTrinh on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AccountKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
